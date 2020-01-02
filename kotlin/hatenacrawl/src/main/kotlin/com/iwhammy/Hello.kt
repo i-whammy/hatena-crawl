aacc6a02674bffb4c53ec47312e5afba365eab12
i-whammy <yas.iwami@gmail.com>
2020-01-02 22:01:43 +0900

    created kotlin project

---
+++ kotlin/hatenacrawl/src/main/kotlin/com/iwhammy/Hello.kt
@@ -0,0 +1,6 @@
+package com.iwhammy
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ kotlin/hatenacrawl/src/test/kotlin/com/iwhammy/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.iwhammy
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
